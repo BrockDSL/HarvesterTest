1c5b0928f9b0f6c4ae18d365db2f38af33faf3e7
DanielBrett <dbrett@brocku.ca>
2022-11-15 16:08:22 -0500

    Update CSVBuilder.py

--- CSVBuilder.py
+++ CSVBuilder.py
@@ -1,33 +1,8 @@
 import os
 import pandas as pd
 
-folder = 'HarvestedReddits'
+folder = 'HarvestedReddits/technology'
 
-
-
-
-df1 = pd.read_json('file1.json')
-
-# view data
-print(df1)
-
-# load json file using pandas
-df2 = pd.read_json('file2.json')
-
-# view data
-print(df2)
-
-# use pandas.concat method
-df = pd.concat([df1,df2])
-
-# view the concatenated dataframe
-print(df)
-
-# convert dataframe to csv file
-df.to_csv("CSV.csv",index=False)
-
-# load the resultant csv file
-result = pd.read_csv("CSV.csv")
-
-# and view the data
-print(result)
+for filename in os.listdir(folder):
+    filejson = pd.read_json(filename)
+    print(filejson)
